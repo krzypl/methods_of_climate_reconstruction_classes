75c3454e8447392612cdfbe0492656eb37ad0391
Krzysztof Pleskot <krzypl@amu.edu.pl>
2024-12-16 14:13:21 +0100

    next commit

--- wapls.R
+++ wapls.R
@@ -1,30 +1,32 @@
 library(rioja)
 library(tidyverse)
+library(analogue)
 
 #data preparation
 dat <- readRDS("data/chiro_dat_spore_kusowo.rds") #load data
 age <- dat$age #extract age
 depth <- dat$depth #extract depth
 
-fos_names <- read_csv("data/fos_names.csv") #load full names 
-
-fos_prep <- dat$fossils
-names(fos_prep) <- fos_names$name
-
-fos <- fos_prep %>% 
-  rename(`Corynoeura coronata-type` = `Corynoneura cornata-type`,
-         `Nanocladius (Nanocladius)` = `Nanocladius rectinervis-type`,
-         `Nanocladius (Plecopterocanthus)` = `Nanocladius branchicolus-type`) %>% 
-  mutate(`Cryptochironomus/Demicryptochironomus` = `Cryptochironomus` + `Demicryptochironomus`,
-         `Glyptotendipes` = `Glyptotendipes barbipes-type` + `Glyptotendipes pallens_severini`) %>% 
-  select(!c(`Chironomini,1st,instar`, `chironomini,undiff`, `Corynoneura,undiff`, `Cricotopus bicinctus-type`,
-            `Cricotopus/Orthocladius`, `Micropsectra,undiff`, `Orthocladiinae,undiff`, `Parakiefferiella triquetra-type`,
-            `Paratanytarsus,undiff`, `Tanypodinae,undiff`, `Tanytarsini,undiff`, `Tribelos`,
-            `Cryptochironomus`, `Demicryptochironomus`, `Glyptotendipes barbipes-type`, `Glyptotendipes pallens_severini`))
-
-harmonized_names <- read_csv("data/harmonized_names.csv")
-
-names(fos) <- harmonized_names$short_name
+# 
+# fos_names <- read_csv("data/fos_names.csv") #load full names 
+# 
+# fos_prep <- dat$fossils
+# names(fos_prep) <- fos_names$name
+# 
+# fos <- fos_prep %>% 
+#   rename(`Corynoeura coronata-type` = `Corynoneura cornata-type`,
+#          `Nanocladius (Nanocladius)` = `Nanocladius rectinervis-type`,
+#          `Nanocladius (Plecopterocanthus)` = `Nanocladius branchicolus-type`) %>% 
+#   mutate(`Cryptochironomus/Demicryptochironomus` = `Cryptochironomus` + `Demicryptochironomus`,
+#          `Glyptotendipes` = `Glyptotendipes barbipes-type` + `Glyptotendipes pallens_severini`) %>% 
+#   select(!c(`Chironomini,1st,instar`, `chironomini,undiff`, `Corynoneura,undiff`, `Cricotopus bicinctus-type`,
+#             `Cricotopus/Orthocladius`, `Micropsectra,undiff`, `Orthocladiinae,undiff`, `Parakiefferiella triquetra-type`,
+#             `Paratanytarsus,undiff`, `Tanypodinae,undiff`, `Tanytarsini,undiff`, `Tribelos`,
+#             `Cryptochironomus`, `Demicryptochironomus`, `Glyptotendipes barbipes-type`, `Glyptotendipes pallens_severini`))
+# 
+# harmonized_names <- read_csv("data/harmonized_names.csv")
+# 
+# names(fos) <- harmonized_names$short_name
 
 ns_ts <- read.table("https://www.ncei.noaa.gov/pub/data/paleo/insecta/chironomidae/europe/heiri2011-swiss-norwegian-calibration.txt", header = TRUE)
 
@@ -33,14 +35,20 @@ spp <- ns_ts %>%
 
 ns_ts_temp <- ns_ts$July.temp
 
-spp_names_wapls <- names(spp)
-fos_names_wapls <- names(fos)
-cols_to_add <- names(spp[!names(spp) %in% fos_names_wapls])
+# spp_names_wapls <- names(spp)
+# fos_names_wapls <- names(fos)
+# cols_to_add <- names(spp[!names(spp) %in% fos_names_wapls])
+# 
+# fos[,cols_to_add] <- 0
+# 
+# fos_wapls <- fos %>% 
+#   select(sort(names(fos)))
+# 
+# write_csv(fos_wapls, "data/example_fos_correct_names.csv")
 
-fos[,cols_to_add] <- 0
+harmonized_names <- read_csv("data/harmonized_names.csv")
 
-fos_wapls <- fos %>% 
-  select(sort(names(fos)))
+fos_wapls <- read_csv("data/example_fos_correct_names.csv")
 
 names(spp) %in% names(fos_wapls) #all names should match in both datasets
 
@@ -55,7 +63,17 @@ rand.t.test(fit.cv)
 screeplot(fit.cv)
 
 #predict the core
-pred <- predict(fit, fos_wapls)
+pred <- predict(fit, fos_wapls, sse = TRUE, nboot = 1000)
+
+ reconPlot(pred$fit.boot[, 2],
+          depth = age,
+          rev.x = TRUE,
+          errors = pred$SEP.boot[, 2],
+          use.labels = TRUE,
+          display.error = "bars",
+          xlab = "Wiek (lata BP)",
+          ylab = "Temperature lata (st. C)")
 
 plot(age, pred$fit[, 2], type="b", ylab="C-IT", las=1)
+
 lines(age, dat$CIT_original, col = "red")
